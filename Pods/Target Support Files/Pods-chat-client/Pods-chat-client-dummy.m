6440a44012de8b9560354bd81873975d35ac866a
Anh-Tu Hoang <hatu87@gmail.com>
2015-11-25 19:06:17 +0700

    init project

---
+++ Pods/Target Support Files/Pods-chat-client/Pods-chat-client-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_chat_client : NSObject
+@end
+@implementation PodsDummy_Pods_chat_client
+@end
---
+++ Pods/Target Support Files/Pods-chat-client/Pods-chat-client-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_chat_clientVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_chat_clientVersionString[];
+
